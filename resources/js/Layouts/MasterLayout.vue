8f346ac87313f0d68339138601242a772cb7c4db
Nafiur Rahman <nrsajib@gmail.com>
2024-12-28 11:43:30 +0600

    assets

---
+++ resources/js/Layouts/MasterLayout.vue
@@ -0,0 +1,44 @@
+<script>
+import Header from '@/Shared/Header.vue';
+import Sidebar from '@/Shared/Sidebar.vue';
+import Footer from '@/Shared/Footer.vue';
+
+export default {
+    components: {
+        Header,
+        Sidebar,
+        Footer,
+    },
+};
+</script>
+<template>
+    <div>
+        <Sidebar />
+        <div class="container-fluid">
+            <div class="main-content d-flex flex-column">
+                <Header />
+                <slot />
+                <Footer />
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- <Header />
+        <div class="d-flex">
+            <Sidebar />
+            <main class="flex-grow-1">
+                <slot />
+            </main>
+        </div>
+        <Footer /> -->
+
+<style scoped>
+.d-flex {
+    display: flex;
+}
+
+.flex-grow-1 {
+    flex-grow: 1;
+}
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Links Of CSS File -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/sidebar-menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/simplebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/apexcharts.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/prism.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/rangeslider.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/quill.snow.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/google-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/fullcalendar.main.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/jsvectormap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/lightpick.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+<body class="boxed-size">
+    @inertia
+
+    <script src="{{ asset('backend/assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/sidebar-menu.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/dragdrop.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/rangeslider.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/quill.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/data-table.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/prism.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/clipboard.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/feather.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/echarts.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/fullcalendar.main.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/jsvectormap.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/world-merc.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/moment.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/lightpick.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/custom/apexcharts.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/custom/echarts.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/custom/custom.js') }}"></script>
+</body>
+</html>
